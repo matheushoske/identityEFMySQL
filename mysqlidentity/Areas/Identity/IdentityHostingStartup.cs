9f195db4a2f801e976deedb0e209b20474626f78
matheushoske <matheushoske@gmail.com>
2021-12-19 05:09:39 -0300

    First Commit

---
+++ mysqlidentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using mysqlidentity.Data;
+
+[assembly: HostingStartup(typeof(mysqlidentity.Areas.Identity.IdentityHostingStartup))]
+namespace mysqlidentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
